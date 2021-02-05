76968cd4f7a559648b934c7e4ee078b179b1739d
ARNDCLZD <64208880+ARNDCLZD@users.noreply.github.com>
2021-02-05 14:54:01 +0100

    initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator anim;
+    public bool isSpirit = false;
+
+    private bool playerOnGround;
+    private void Update()
+    {
+        if (!isSpirit)
+        {
+            if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.Z))
+            {
+                if (Input.GetKey(KeyCode.Q))
+                {
+                    gameObject.GetComponent<SpriteRenderer>().flipX = true;
+                    anim.SetBool("running", true);
+                    rb.velocity = new Vector2(-10, rb.velocity.y);
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    gameObject.GetComponent<SpriteRenderer>().flipX = false;
+                    anim.SetBool("running", true);
+                    rb.velocity = new Vector2(10, rb.velocity.y);
+                }
+                if (Input.GetKey(KeyCode.Z) && playerOnGround)
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, 7f);
+                    anim.SetBool("jumping", true);
+                    playerOnGround = false;
+                }
+            }
+            else
+            {
+                anim.SetBool("jumping", false);
+                anim.SetBool("running", false);
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                isSpirit = true;
+            }
+            /*if (Input.GetKey(KeyCode.S))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -2);
+            }*/
+        } else
+        {
+            anim.SetBool("jumping", false);
+            anim.SetBool("running", false);
+        }     
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            playerOnGround = true;
+        }
+    }
+
+}
---
+++ Assets/PlayerSoulController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoulController : MonoBehaviour
+{
+    public  GameObject player;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), this.GetComponent<Collider2D>()); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(0, 0);
+        Debug.Log("Mort : " + this.GetComponentInParent<PlayerController>().isSpirit);
+        if (this.GetComponentInParent<PlayerController>().isSpirit)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                this.GetComponentInParent<PlayerController>().isSpirit = false;
+            }
+            if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.S))
+            {
+                if (Input.GetKey(KeyCode.Q))
+                {
+                    rb.velocity = new Vector2(-10, rb.velocity.y);
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    rb.velocity = new Vector2(10, rb.velocity.y);
+                }
+                if (Input.GetKey(KeyCode.Z))
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, 7f);
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, -7f);
+                }
+            }
+        }
+        else
+        {
+            transform.position = new Vector2(player.transform.position.x, player.transform.position.y);
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
