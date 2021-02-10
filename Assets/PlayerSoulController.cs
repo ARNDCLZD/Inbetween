151aa10cc2894537b5525dd65381270a5c07a8e5
barthelemy GODART <barthelemy.godart11@gmail.com>
2021-02-10 18:26:05 +0100

    BasicTrigger

--- Assets/PlayerSoulController.cs
+++ Assets/PlayerSoulController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerSoulController : MonoBehaviour
 {
-    public  GameObject player;
+    public GameObject player;
     public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
